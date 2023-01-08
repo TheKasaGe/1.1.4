923d20cde30dcfb61bdc97d2f1f4d5d0f7cd6a1b
TheKasaGe <118396007+TheKasaGe@users.noreply.github.com>
2023-01-08 11:56:09 +0300

    Внедрение дао в сервис через поле

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,33 +7,28 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDao dao = new UserDaoJDBCImpl();
     public void createUsersTable() {
-        UserDao dao = new UserDaoJDBCImpl();
         dao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDao dao = new UserDaoJDBCImpl();
         dao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDao dao = new UserDaoJDBCImpl();
         dao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDao dao = new UserDaoJDBCImpl();
         dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDao dao = new UserDaoJDBCImpl();
         return dao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDao dao = new UserDaoJDBCImpl();
         dao.cleanUsersTable();
     }
 }
